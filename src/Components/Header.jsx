e75de3cf309ac5a69c86f5257219ea0870b5ce59
alexanderreinholz <alexander.reinholz@icloud.com>
2024-04-23 01:13:21 +0200

    Creating the structure by adding semantic html + adding meta tags

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+
+function Header() {
+    return (
+        <>
+            <img src={logo} className="App-logo" alt="logo"/>
+        </>
+    );
+}
+
+export default Header;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+function Nav() {
+    return (
+        <>
+            <ul>
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="">Page2</a>
+                </li>
+                <li>
+                    <a href="">Page3</a>
+                </li>
+            </ul>
+        </>
+    );
+}
+
+export default Nav;
